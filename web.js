ce1b5432b75febe89a864669443bb87070ac4846
vktech42 <vktech42@gmail.com>
2025-04-22 21:23:44 +0530

    cross and bar create

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
-let icon=document.querySelector(".icon")
-let ul=document.querySelector("ul")
-icon.addEventListener("click",()=>{
-    ul.classList.toggle("showdata")
-    console.log(uL)
-})
+let icon = document.querySelector(".icon");
+let ul   = document.querySelector("ul");
 
+icon.addEventListener("click", () => {
+  ul.classList.toggle("showdata");
 
-
+  const bar = document.getElementById("bar");
+  if (ul.classList.contains("showdata")) {
+    bar.className = "fa-solid fa-xmark";
+  } else {
+    bar.className = "fa-solid fa-bars";
+  }
+});
